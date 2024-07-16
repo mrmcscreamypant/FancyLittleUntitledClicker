f70b6abc7c7b79bbc68783c096bb7a12fa008ec6
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2024-07-16 02:45:44 +0000

    Basic server system

---
+++ main.py
@@ -0,0 +1,15 @@
+from bottle import route,run
+
+def get_file(filename):
+  with open(f"/client/{filename}","r") as file:
+    return file.read()
+
+@route("/")
+def homepage():
+  return page("index.html")
+
+@route("/<path:path>")
+def page(path):
+  return get_file(path)
+
+run(host="0.0.0.0",port=8080,debug=True)
