e817f77c274e017471a6b79b1e30adc893c17f38
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2024-07-16 03:55:15 +0000

    Still fighting with window CSS

--- client/index.js
+++ client/index.js
@@ -1,8 +1,38 @@
-class Tier {
-  
+class WindowOptions extends Array {
+  constructor(...args) {
+    super()
+
+    this.hidden = true
+    this.class = ["no-full","no-max"]
+    
+    for (var i=0;i<args.length;i++) {
+      this[args.entries()[i]] = args[args.entries()[i]]
+    }
+  }
 }
 
-class Tier1 extends Tier
+class Tier {
+  constructor() {
+    this._windows = {}
+  }
+
+  newWindow(id,title,options={}) {
+    options = new WindowOptions(options)
+    this._windows[id] = (new WinBox(title,options))
+  }
+}
+
+class Tier1 extends Tier {
+  constructor() {
+    super()
+    
+    this.newWindow("button","Button",{})
+  }
+
+  start() {
+    this._windows.button.show()
+  }
+}
 
 function startup() {
   const tier1 = new Tier1()
